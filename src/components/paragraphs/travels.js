3831dfaec2b8d8798df421ad5043702eeac7f9cf
Jordan Sassoon <j.sassoon@student.tudelft.nl>
2023-01-28 13:07:52 +0100

    update pics

--- src/components/paragraphs/travels.js
+++ src/components/paragraphs/travels.js
@@ -2,14 +2,14 @@ import React from 'react';
 import whitsundays from './photos/whitsundays.jpg';
 import car from './photos/car.jpg';
 import smoke from './photos/smoke.jpg';
-import shops from './photos/shops.jpg';
+import gibsons from './photos/gibsons.jpg';
 
 function Travels() {
   return (
     <div className="vert">
       <div className="horiz">
         <img src={smoke} alt="Magnus"></img>
-        <img src={shops} alt="Shops"></img>
+        <img src={gibsons} alt="Gibsons"></img>
       </div>
       <div className="horiz">
         <img src={whitsundays} alt="Whitsundays"></img>
