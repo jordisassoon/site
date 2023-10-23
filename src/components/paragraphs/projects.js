8c53bb776c30ab6fd1a4f87e6416f8e3a4276488
Jordan <43263941+jordisassoon@users.noreply.github.com>
2023-10-23 17:50:18 +0200

    Update projects.js

--- src/components/paragraphs/projects.js
+++ src/components/paragraphs/projects.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 function Projects() {
   const text = 
-  "Bachelor of Computer Science and Engineering," + "\n" +
-  "TU Delft - Undergoing" + "\n\n" +
+  "Master of Data and Artificial Intelligence" + "\n" +
+  "Insitut Polytechnique de Paris - Undergoing" + "\n\n" +
   "Researcher and Developer" + "\n" +
   "CV Lab (TU Delft) - Undergoing" + "\n\n" +
+  "Bachelor of Computer Science and Engineering" + "\n" +
+  "TU Delft - 2023" + "\n\n" +
   "Minor Abroad" + "\n" +
   "Queensland University of Technology - 2022" + "\n\n" +
   "AI Developer" + "\n" +
